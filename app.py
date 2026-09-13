105b4f417ab834b6f5a23b721e0161f063b7a5bf
toxiccode007 <toxiccode007@gmail.com>
2026-09-13 19:23:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, send_from_directory
-import os
+from pathlib import Path
 
 app = Flask(__name__)
 
@@ -10,10 +10,10 @@ def index():
 @app.route("/download")
 def download():
     return send_from_directory(
-        os.path.join(app.root_path, "challenge_files"),
+        Path(__file__).parent / "challenge_files",
         "recovered_archive.zip",
         as_attachment=True
     )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=5000)
